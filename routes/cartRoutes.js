b8d3f8e3f68816908faff7329e94577e1230663f
Richie <molunorichie@gmail.com>
2022-09-11 20:24:55 +0100

    add: checkout route

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -8,6 +8,6 @@ router.
     post("/add", cart.addPropertyToCart).
     post("/remove", cart.removePropertyFromCart).
     post("/clear", cart.clearCart).
-    post("/checkout", cart.checkoutCart);
+    post("/checkout", cart.checkout);
 
 module.exports = router;
