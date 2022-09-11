5d14031c97ad55862b6c45f3d841c556ddaa28b1
Richie <molunorichie@gmail.com>
2022-09-11 20:23:50 +0100

    update: specification price type to Number

--- models/propertyModel.js
+++ models/propertyModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const specificationsSchema = new Schema({
-    price: { type: String },
+    price: { type: Number },
     bedrooms: { type: String },
     bathrooms: { type: String },
     area: { type: String }
